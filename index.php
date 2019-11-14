896e103858ebc9fb19120c9dd40c4f9c5d9cf366
MuddySpoon <36851510+MuddySpoon@users.noreply.github.com>
2019-11-14 15:41:09 +0700

    a

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Main Menu</title>
+    <link rel="stylesheet" href="Main Menu.css">
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+    <body>
+        <div class="logo w-100 h-25">
+            <img class="w-100 h-100" src="LogoCAP.PNG" alt="Logo CAP2019">
+        </div>
+        <div class="Informasi">
+            <p>Silahkan masukkan data sesuai didalam nametag</p>
+        </div>
+        <div class="InputData">
+            <div class="InputUsername">
+                <input type="text" id="Username" placeholder="Username">
+            </div>
+            <div class="InputNomorKelompok">
+                <input type="number" id="NomorKelompok" placeholder="Nomor Kelompok">
+            </div>
+        </div>
+        <div class="button">
+            <button class="SignIn">
+                Sign In
+            </button>
+        </div>
+    </body>
+</html>
