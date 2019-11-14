15a32d91e7aa69f95a863b967e0be336cbcf0507
MuddySpoon <muddyspoonn@gmail.com>
2019-11-14 17:17:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
 </head>
     <body>
         <div class="logo w-100 h-25">
-            <img class="LogoCAP" src="LogoCAP.png" alt="Logo CAP2019">
+            <?php 
+                $image_url='LogoCAP.PNG';
+            ?>
+
+            <img class="LogoCAP" src="<?php echo $image_url;?>">
         </div>
         <div class="Informasi">
             <p>Silahkan masukkan data sesuai didalam nametag</p>
