8dd5773c26ab6c9ac46609ad5b9fac567d31a673
MuddySpoon <muddyspoonn@gmail.com>
2019-11-14 17:25:33 +0700

    Revert "Update index.php"
    This reverts commit 15a32d91e7aa69f95a863b967e0be336cbcf0507.

--- index.php
+++ index.php
@@ -10,11 +10,7 @@
 </head>
     <body>
         <div class="logo w-100 h-25">
-            <?php 
-                $image_url='LogoCAP.PNG';
-            ?>
-
-            <img class="LogoCAP" src="<?php echo $image_url;?>">
+            <img class="LogoCAP" src="LogoCAP.png" alt="Logo CAP2019">
         </div>
         <div class="Informasi">
             <p>Silahkan masukkan data sesuai didalam nametag</p>
