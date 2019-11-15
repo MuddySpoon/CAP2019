dad0a514b4185cfcdf83f6c0e91e8e0ab8d562c7
MuddySpoon <muddyspoonn@gmail.com>
2019-11-15 15:27:26 +0700

    Edit Wahana Page

--- index.php
+++ index.php
@@ -7,6 +7,30 @@
     <title>Main Menu</title>
     <link rel="stylesheet" href="Main Menu.css">
     <link rel="stylesheet" href="bootstrap.css">
+
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-analytics.js"></script>
+
+    <script>
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDg15qeT7tEkX1lJXK7iYlbru7o9WNWY6Y",
+        authDomain: "cap-2019-832ba.firebaseapp.com",
+        databaseURL: "https://cap-2019-832ba.firebaseio.com",
+        projectId: "cap-2019-832ba",
+        storageBucket: "cap-2019-832ba.appspot.com",
+        messagingSenderId: "77391295035",
+        appId: "1:77391295035:web:dab001ddfc1c5e07a2e21e",
+        measurementId: "G-C991JYDZYE"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    </script>
 </head>
     <body>
         <div class="logo w-100 h-25">
@@ -15,14 +39,16 @@
         <div class="Informasi">
             <p>Silahkan masukkan data sesuai didalam nametag</p>
         </div>
-        <div class="InputData">
-            <div class="InputUsername">
-                <input type="text" id="Username" placeholder="Username">
+        <form action="">
+            <div class="InputData">
+                <div class="InputUsername">
+                    <input type="text" id="Username" placeholder="Username">
+                </div>
+                <div class="InputNomorKelompok">
+                    <input type="number" id="NomorKelompok" placeholder="Nomor Kelompok">
+                </div>
             </div>
-            <div class="InputNomorKelompok">
-                <input type="number" id="NomorKelompok" placeholder="Nomor Kelompok">
-            </div>
-        </div>
+        </form>
         <div class="button">
             <button class="SignIn">
                 Sign In
