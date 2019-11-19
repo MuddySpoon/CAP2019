0bd932c876e397b9b20b6c4b17b2c81dec1f6de6
MuddySpoon <muddyspoonn@gmail.com>
2019-11-19 15:47:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,13 @@
     <title>Main Menu</title>
     <link rel="stylesheet" href="Main Menu.css">
     <link rel="stylesheet" href="bootstrap.css">
-    <script language="Javascript" type="text/javascript" src="main.js"></script>
+    <script src="main.js"></script>
     <script src='https://www.gstatic.com/firebasejs/7.4.0/firebase.js'></script>
     
     <script src='https://www.gstatic.com/firebasejs/7.4.0/firebase-database.js'></script>
 
     <!-- The core Firebase JS SDK is always required and must be listed first -->
     <script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
-
-    <!-- TODO: Add SDKs for Firebase products that you want to use
-        https://firebase.google.com/docs/web/setup#available-libraries -->
-    <script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-analytics.js"></script>
 
     <script>
     // Your web app's Firebase configuration
@@ -33,7 +29,6 @@
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
     </script>
 </head>
     <body>
