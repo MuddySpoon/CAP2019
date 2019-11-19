52d53f6cd957fa6bd28eb094ebe2ed2f78d006c2
MuddySpoon <muddyspoonn@gmail.com>
2019-11-19 22:17:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,17 +5,14 @@ var database = firebase.database();
 function submitClick(){
 
     var Username = document.getElementById("Username").value;
-    console.log(Username);
     if(Username == ""){
-        console.log("askodkasod")
         alert("Username harus diisi!");
         return false;
-      
     }
     else{
         valUsername = document.getElementById("Username").value;
         writeUserData(valUsername);
-        window.location.href = ("https://cappppp.herokuapp.com/MainGame.html")
+        window.location.href = ("MainGame.html")
     }
 }
 
