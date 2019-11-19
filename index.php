334621f1e548d734dfcae92c56b314328b3fd0f2
MuddySpoon <muddyspoonn@gmail.com>
2019-11-19 09:06:18 +0700

    test

--- index.php
+++ index.php
@@ -43,10 +43,10 @@
         <form method="post" action="MainGame.html">
             <div class="InputData">
                 <div class="InputUsername">
-                    <input type="text" id="Username" placeholder="Username">
+                    <input type="text" id="Username" placeholder="Username" require>
                 </div>
                 <div class="InputNomorKelompok">
-                    <input type="number" id="NomorKelompok" placeholder="Nomor Kelompok">
+                    <input type="number" id="NomorKelompok" placeholder="Nomor Kelompok" require>
                 </div>
             </div>
         
