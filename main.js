5b9f7ec98391b93d090107886a5a93ed058d4787
MuddySpoon <muddyspoonn@gmail.com>
2019-11-19 09:04:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ function submitClick(){
     }
     if(groupnumber == ""){
         alert("Nomor kelompok harus diisi!");
+        return false;
     }
     if(groupnumber == 0 || groupnumber > 20){
         alert("Perhatikan nomor kelompok anda!");
+        return false;
     }
     else{
         //document.getElementById("GetUsername").innerHTML = username;
