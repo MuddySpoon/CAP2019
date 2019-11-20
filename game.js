e554ba551d6191c132791365c5d4c17a16b90b79
MuddySpoon <muddyspoonn@gmail.com>
2019-11-20 22:09:02 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,25 +1,25 @@
 var database = firebase.database;
 var getUserID = document.getElementById("getUserID");
-var currUsername = localStorage.getItem("Username");
 var database = firebase.database();
-var childData;
+
+
+// var childData;
+// var currUsername = localStorage.getItem("Username");
+// window.onload = function getUserID(){
+//     firebase.database().ref('users/' + currUsername).on('value',
+//     function(snapshot) {
+//         snapshot.forEach(function(childSnapshot) {
+//         childData = childSnapshot.val();
+//         });
+//     });
+//     console.log(childData);
+// }
 // console.log(getUserID);
 // getUserID.innerHTML = localStorage.getItem("Username");
 
 
-// var firebasegetUserID = firebase.database().ref().child("UserID");
-// firebasegetUserID.on('value',  function(datasnapshot){
-//     getUserID.innerText = datasnapshot.
-// })
-
-window.onload = function getUserID(){
-    firebase.database().ref('users/' + currUsername).on('value',
-    function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-        childData = childSnapshot.val();
-        });
-    });
-    console.log(childData);
-}
-
+var firebasegetUserID = firebase.database().ref().child("UserID");
+firebasegetUserID.on('value',  function(datasnapshot){
+    getUserID.innerText = datasnapshot.valUsername();
+})
 //document.getElementById("getUserID").innerHTML = firebase.database()     ;
